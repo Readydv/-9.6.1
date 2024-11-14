e238961771518dfb17cfc8503aa00de3e1db7a95
Danil <daniil-vn@mail.ru>
2024-11-14 05:22:35 +0500

    Задание 1

--- Задание 9.6.1/Program.cs
+++ Задание 9.6.1/Program.cs
@@ -1,1 +1,44 @@
-﻿
+﻿using System;
+using System.IO;
+
+namespace Task
+{
+    public class MyException : Exception 
+        { 
+            public MyException(string message) : base(message) 
+            { 
+
+            }
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exceptions = new Exception[]
+            {
+                new ArgumentNullException("Параметр не может быть null"),
+                new FormatException("Значение введено некорректно"),
+                new FileNotFoundException("Файл не существует"),
+                new DriveNotFoundException("Диск недоступен или не существует"),
+                new DirectoryNotFoundException("Недопустимая часть пути к каталогу")
+            };
+
+            foreach (var exception in exceptions)
+            {
+                try
+                {
+                    throw exception;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Поймано исключение: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Блок finally был выполнен");
+                }
+            }
+        }
+    }
+}
